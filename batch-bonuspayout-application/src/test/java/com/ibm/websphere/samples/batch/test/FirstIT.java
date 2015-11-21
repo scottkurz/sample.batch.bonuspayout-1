71af581ba7e6757ebb3bf0627013e557ba2855ef
Scott Kurz <skurz@us.ibm.com>
2015-11-20 22:51:14 -0500

    Next

--- batch-bonuspayout-application/src/test/java/com/ibm/websphere/samples/batch/test/FirstIT.java
+++ batch-bonuspayout-application/src/test/java/com/ibm/websphere/samples/batch/test/FirstIT.java
@@ -19,12 +19,16 @@ package com.ibm.websphere.samples.batch.test;
 import static net.sf.expectit.filter.Filters.removeNonPrintable;
 import static net.sf.expectit.matcher.Matchers.contains;
 import static net.sf.expectit.matcher.Matchers.eof;
+import static net.sf.expectit.matcher.Matchers.regexp;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.acl.Group;
+
+import javax.batch.runtime.BatchStatus;
 
 import org.junit.After;
 import org.junit.Assume;
@@ -48,6 +52,11 @@ public class FirstIT {
 	private String wlpUserDir = System.getProperty("wlp.user.dir");
 	private String serverHost = System.getProperty("serverHost");
 	private String httpsPort = System.getProperty("httpsPort");
+
+	private String CORE_COMMAND_PARMS =
+			"--batchManager=" +	serverHost + ":" + httpsPort + " " +
+					"--trustSslCertificates --user=bob --password=bobpwd " + 			
+					"--wait --pollingInterval_s=2 ";
 
 	@BeforeClass
 	public static void ignoreOnNonWindows() {
@@ -75,21 +84,61 @@ public class FirstIT {
 	}
 
 	@Test
-	public void testBP() throws Exception {
+	public void testRunToCompletion() throws Exception {
 		String submitCmd = wlpInstallDir + "/bin/batchManager submit " +
-	            "--batchManager=" +	serverHost + ":" + httpsPort + " " +
-				"--trustSslCertificates --user=bob --password=bobpwd " + 
-				//"--applicationName=batch-bonuspayout-application --jobXMLName=BonusPayoutJob " + 
-				"--applicationName=" + warName + " --jobXMLName=BonusPayoutJob " +
-				"--jobPropertiesFile=" + wlpUserDir + "/shared/resources/runToCompletionParms.txt " +
-				"--wait --pollingInterval_s=2";
+				CORE_COMMAND_PARMS +   
+				"--jobXMLName=BonusPayoutJob --applicationName=" + warName + " " +
+				"--jobPropertiesFile=" + wlpUserDir + "/shared/resources/runToCompletionParms.txt ";
+
 		expect.sendLine(submitCmd);
+
+		assertJobTerminationStatus(BatchStatus.COMPLETED);
 
 		expect.sendLine("exit");
 		// expect the process to finish
 		expect.expect(eof());
 	}
 
+	@Test
+	public void testForceFailure() throws Exception {
+
+		String submitCmd = wlpInstallDir + "/bin/batchManager submit " +
+				CORE_COMMAND_PARMS +   
+				"--jobXMLName=BonusPayoutJob --applicationName=" + warName + " " +
+				"--jobPropertiesFile=" + wlpUserDir + "/shared/resources/forceFailureParms.txt ";
+
+		expect.sendLine(submitCmd);
+
+		long jobInstanceId = assertJobTerminationStatus(BatchStatus.FAILED);
+
+		// It doesn't exactly force a failure every time, just after a certain # of records, but we've
+		// gotten far enough to complete on the 1st restart.
+		String restartCmd = wlpInstallDir + "/bin/batchManager restart " +
+				CORE_COMMAND_PARMS + 
+				"--jobInstanceId=" + jobInstanceId + " " +
+				"--jobPropertiesFile=" + wlpUserDir + "/shared/resources/forceFailureParms.txt ";
+		
+		expect.sendLine(restartCmd);
+		assertJobTerminationStatus(BatchStatus.COMPLETED);
+		
+		expect.sendLine("exit");
+		// expect the process to finish
+		expect.expect(eof());
+	}
+
+	/**
+	 * @param batchStatus Expected status
+	 * @return job instance ID
+	 * @throws IOException
+	 */
+	private long assertJobTerminationStatus(BatchStatus batchStatus) throws IOException {
+		// CWWKY0105I: Job {0} with instance ID {1} has finished. Batch status: {2}. Exit status: {3}
+		Result res = expect.expect(regexp("CWWKY0105I: Job (.*) with instance ID (\\d+) has finished. Batch status: ([A-Z]+)"));	
+		assertEquals("Job terminated, but in unexpected state", batchStatus.toString(), res.group(3));
+		return Long.parseLong(res.group(2));
+	}
+
+	/*
 	@Test
 	@Ignore
 	public void test() throws IOException, InterruptedException {
@@ -105,4 +154,5 @@ public class FirstIT {
 		expect.sendLine("exit");
 		assertTrue(expect.expect(eof()).isSuccessful());
 	}
+	 */
 }
